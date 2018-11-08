a226c151eac469bd87c2156af35d9992c447cbf1
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2018-11-08 23:26:55 +0900

    diff_files, pull_requests

--- lib/sakanax/cli.rb
+++ lib/sakanax/cli.rb
@@ -5,12 +5,24 @@ require_relative 'github'
 
 module Sakanax
   class CLI < Thor
-    desc "files", "Detect files included in the PullRequest."
-    def files
-      puts "detect files.."
 
+    default_command :pull_requests
+    desc "pull_requests", "A list of PullRequests containing the target commit ID (sha)."
+    option "sha", required: true
+    def pull_requests
       github = Github.new
-      github.get_pull_requests
+      p github.get_pull_requests_contained_target_commit_id(options['sha'])
     end
+
+    desc "files", "Detect files included in the PullRequests containing the target commit ID (sha)."
+    option "sha", required: true
+    def diff_files
+      github = Github.new
+      pull_requests =  github.get_pull_requests_contained_target_commit_id(options['sha'])
+      pull_requests.each do |pr|
+        p "PullRequest: #{pr}, diff files: #{github.get_files_with_changes(pr)}"
+      end
+    end
+
   end
 end
--- lib/sakanax/github.rb
+++ lib/sakanax/github.rb
@@ -2,15 +2,39 @@ class Github
 
   def initialize()
     @client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
+    @repository = 'sakanax/sakanax'
   end
 
-  def get_pull_requests()
-    repository_name = 'daisuke-awaji/aws-continuous-integration-flamework'
-    pull_requests = @client.pull_requests(repository_name)
-    puts pull_requests
+  # Open状態のPullRequestの一覧を配列で取得する
+  def get_opened_pull_requests()
+    pull_requests = @client.pull_requests(@repository)
+
+    opened_pull_requests = Array.new
+    pull_requests.each do |pr|
+      opened_pull_requests.push(pr[:number])
+    end
+    return opened_pull_requests
   end
 
-  def hello
-    puts "hello"
+  # コミットID(sha)を含むPullRequestの一覧を配列で取得する
+  def get_pull_requests_contained_target_commit_id(sha)
+    pull_requests_contained_target_commit_id = Array.new
+    get_opened_pull_requests().each do |pr|
+      @client.pull_request_commits(@repository, pr).each do |commit|
+        pull_requests_contained_target_commit_id.push(pr) if sha == commit[:sha]
+      end
+    end
+    p "[INFO] target commit ID (sha: #{sha}) does not exist in the currently open PullRequests." if pull_requests_contained_target_commit_id.empty?
+    return pull_requests_contained_target_commit_id
+  end
+
+  # テスト対象とするPRの番号から、変更があったファイルを抽出する
+  def get_files_with_changes(pull_request)
+    files = Array.new
+    @client.pull_request_files(@repository, pull_request).each do |file|
+      files.push(file[:filename])
+    end
+    p '[INFO] No file changed in the specified PullRequest' if files.empty?
+    return files
   end
 end
